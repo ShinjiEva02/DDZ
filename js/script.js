11a9e1018e2a5243c8e07be28366ed8f5777cc14
Dima Sazonov <mister.mitya99@gmail.com>
2018-11-12 13:48:56 +0300

    add Map and markers

--- js/script.js
+++ js/script.js
@@ -10,8 +10,34 @@ window.addEventListener('DOMContentLoaded', () => {
         
         if(request.readyState == 4 && request.status == 200) {
             data = JSON.parse(request.response);
-            document.body.innerHTML = data;
-            console.log("Все класс");
+            
+            var mymap = L.map('mapid').setView([53.682374, 23.835182], 13);
+
+            L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
+                maxZoom: 18,
+                attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
+                    '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
+                    'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+                id: 'mapbox.streets'
+            }).addTo(mymap);
+    
+            for(let i = 0; i < data.length; i++) {
+                L.marker(data[i].gps).addTo(mymap)
+                    .bindPopup(`<b>${data[i].name}</b><br /> <img src="${data[i].url}" alt="${data[i].name}">`).openPopup();    
+            }
+            
+            var popup = L.popup()
+                .setLatLng([53.682374, 23.835182])
+                .setContent("I am Hrodno.")
+                .openOn(mymap);
+            function onMapClick(e) {
+                popup
+                    .setLatLng(e.latlng)
+                    .setContent("You clicked the map at " + e.latlng.toString())
+                    .openOn(mymap);
+            }
+    
+            mymap.on('click', onMapClick);
         }
     });
 
@@ -22,4 +48,5 @@ window.addEventListener('DOMContentLoaded', () => {
     //         this.url = url;
     //     }
     // }
+
 });
